27d0127c80b0f3f79889b671677da3a5e2274fe8
jun <1004ji93@gmail.com>
2024-10-22 20:25:47 +0900

    setting

---
+++ src/Components/SignIn.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const SignIn = () => {
+  return (
+    <div className="signin-form">
+      <button>Sig in with Google</button>
+    </div>
+  );
+};
+export default SignIn;
